80a4de27326deda594eb6ecf620336aab53fa3cf
NQBH <nguyenquanbahong@gmail.com>
2023-08-15 17:44:30 +0700

    Python: min square of ints

--- elementary_computer_science/Python/square_1.py
+++ elementary_computer_science/Python/square_1.py
@@ -1,9 +1,7 @@
 n = int(input())
-a = [0]*n
-b = [0]*n
+a = [0]*n; b = [0]*n
 for i in range(n):
 	a[i], b[i] = map(int, input().split())
-a.sort()
-b.sort()
+a.sort(); b.sort()
 c = max(a[n-1] - a[0], b[n-1] - b[0])
 print(c*c)
