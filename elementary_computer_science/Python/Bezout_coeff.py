c15d670face7bb46e37f3ef573150f7ded9fbaa8
NQBH <nguyenquanbahong@gmail.com>
2024-01-23 11:37:52 +0700

    dynamical gcd: O(log a + log b)

---
+++ elementary_computer_science/Python/Bezout_coeff.py
@@ -0,0 +1,9 @@
+def Bezout_coeff(a, b):
+	if b == 0:
+		return (1, 0)
+	u, v = Bezout_coeff(b, a % b)
+	return (v, u - (a // b) * v)
+def inv(a, p):
+	return Bezout_coeff(a, p)[0] % p
+a, b = map(int, input().split())
+print(Bezout_coeff(a, b))
