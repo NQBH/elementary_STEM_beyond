6d5f2dc22816a19673419540aa7d1ac1887a63b5
NQBH <nguyenquanbahong@gmail.com>
2024-01-23 17:30:21 +0700

    detect anagram in string

--- elementary_computer_science/Python/anagram.py
+++ elementary_computer_science/Python/anagram.py
@@ -1,4 +1,4 @@
-def anagrams(S): # S is a set of strings
+def anagrams(S): # O(n^2 k log k) # S is a set of strings
 	d = {} 		 # maps s to list of words with signature s
 	for word in S: # group words according to the signature
 		s = ''.join(sorted(word)) # calculate the signature
