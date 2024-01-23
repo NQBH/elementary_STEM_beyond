dd2c6bd8169ca01513b3180aa48281bebd208113
NQBH <nguyenquanbahong@gmail.com>
2024-01-23 11:56:40 +0700

    Bezout coefficient O(log a + log b)

--- elementary_computer_science/Python/Bezout_coeff.py
+++ elementary_computer_science/Python/Bezout_coeff.py
@@ -3,7 +3,9 @@ def Bezout_coeff(a, b):
 		return (1, 0)
 	u, v = Bezout_coeff(b, a % b)
 	return (v, u - (a // b) * v)
+
 def inv(a, p):
 	return Bezout_coeff(a, p)[0] % p
+
 a, b = map(int, input().split())
 print(Bezout_coeff(a, b))
