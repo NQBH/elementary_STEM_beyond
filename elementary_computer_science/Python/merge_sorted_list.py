e319a0157879c0003b32a10a83350b76dae22ef0
NQBH <nguyenquanbahong@gmail.com>
2024-01-24 10:34:23 +0700

    merge sorted lists

--- elementary_computer_science/Python/merge_sorted_list.py
+++ elementary_computer_science/Python/merge_sorted_list.py
@@ -1,7 +1,6 @@
 def merge(x, y): # O(n log n)
 	z = []
-	i = 0
-	j = 0
+	i, j = 0, 0
 	while i < len(x) or j < len(y):
 		if j == len(y) or i < len(x) and x[i] <= y[j]: # priority on x
 			z.append(x[i])
@@ -10,6 +9,7 @@ def merge(x, y): # O(n log n)
 			z.append(y[j]) # now switch to y
 			j += 1
 	return z
+
 x = list(map(int, input().split()))
 y = list(map(int, input().split()))
 print(merge(x, y))
