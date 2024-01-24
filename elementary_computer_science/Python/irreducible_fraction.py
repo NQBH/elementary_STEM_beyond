ddadff07a2ece16de8fcee8b20091054c04be144
NQBH <nguyenquanbahong@gmail.com>
2024-01-24 10:38:01 +0700

    irreducible fraction:

--- elementary_computer_science/Python/irreducible_fraction.py
+++ elementary_computer_science/Python/irreducible_fraction.py
@@ -1,16 +1,11 @@
 from math import gcd
 
 def irreducible_fraction(a, b):
-	if b == 0:
-		print('error: divisible by zero')
-		exit()
+	assert b != 0 # error: div by 0
 	if b < 0: # switch to positive denominator
 		a, b = -a, -b
 	if gcd(a, b) > 1:
-		print('reducible')
 		a, b = a // gcd(a, b), b // gcd(a, b)
-	else:
-		print('already irreducible')
 	return a, b # return str(a) + '/' + str(b)
 
 a, b = map(int, input().split())
