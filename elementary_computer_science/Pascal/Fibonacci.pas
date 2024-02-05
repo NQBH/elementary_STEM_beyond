52a1a58e38e2ac0f5596c1220079e014227493bb
NQBH <nguyenquanbahong@gmail.com>
2024-02-05 11:18:07 +0700

    Fibonacci sequence

---
+++ elementary_computer_science/Pascal/Fibonacci.pas
@@ -0,0 +1,14 @@
+function Fibonacci(n: longint): longint;
+var fi1, fi2, fi, i: longint;
+begin
+	if n <= 1 then exit(n);
+	fi2 := 0;
+	fi1 := 1;
+	for i := 2 to n do
+	begin
+		fi := fi1 + fi2;
+		fi2 := fi1;
+		fi1 := fi;
+	end;
+	exit(fi);
+end;
